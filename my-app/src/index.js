5a7035c71256ef430b9d92194611b59beef25dde
Maxim <maxidebox@list.ru>
2021-10-14 14:48:37 +0400

    Элемент и входные параметры

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,45 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Welcome(props) {
+    return (
+      <p>Hello {props.user.name} {props.user.surname}!</p>
+    );
+}
+
+// class Welcome extends React.Component {
+//    render () {
+//        return <p>Hello {this.props.user.name} {this.props.user.surname}!</p>
+//    }
+// }
+
+function Appnew(props) {
+    const users = [
+      {name: "Alexey", surname: "test1"},
+      {name: "Maxim", surname: "test2"},
+      {name: "Pasha", surname: "test3"},
+    ];
+
+    return (
+        <div>
+            <Welcome user={users[0]} />
+            <Welcome user={users[1]} />
+            <Welcome user={users[2]} />
+        </div>
+    );
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Appnew />,
+  document.querySelector("#root")
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
