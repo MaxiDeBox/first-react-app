bcd534280ab55fdb34b28b483b83ada8852db8a3
Maxim <maxidebox@list.ru>
2021-11-15 12:18:52 +0400

    Поднятие состояния

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,93 +1,55 @@
 import './App.css';
 import React, {useState} from "react";
+import CitiesList from "./CitiesList";
+import City from "./City";
 
-function App(props) {
-  const [value, setValue] = useState("Some text");
-  const [checked, setChecked] = useState(false);
-  const [fruit, setFruite] = useState(false);
-  const [fruitSelect, setSelectedFruite] = useState("");
+function App() {
+  const [cities, setCities] = useState([
+    {
+      id: 0,
+      name: "Moscow",
+      description: "More info about Moscow"
+    },
+    {
+      id: 1,
+      name: "Samara",
+      description: "More info about Samara"
+    },
+    {
+      id: 2,
+      name: "Piter",
+      description: "More info about Piter"
+    }
+  ]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentCity = cities[currentIndex];
 
-  const fruits = [
-    "Apple",
-    "Banana",
-    "Orange"
-  ];
+  const handlerChangeCity = (n, description) => {
+    setCities(cities.map((city, index) => {
+      if (index === n) {
+        return {
+          ...city,
+          description: description
+        }
+      }
 
-  const handlerSelect = (event) => {
-    return (
-      setSelectedFruite(event.target.value)
-    );
-  };
+      return city;
+    }))
+  }
+
+  const handlerselectCity = (n) => {
+    setCurrentIndex(n)
+  }
 
   return (
     <div className="App">
-      <h1>Elements Forms</h1>
-      <input value={value} onChange={(e) => {setValue(e.target.value)}} />
-      <br/>
-      <textarea value={value} onChange={(e) => {setValue(e.target.value)}} />
-      <br/>
-      {value}
-      <hr />
-      <label>
-        <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} />
-      </label>
-      <br />
-      {checked}
-      <hr />
-      <ul>
-        {fruits.map((item, index) => (
-          <li key={item}>
-            <label>
-              <input
-                type="radio"
-                checked={fruit === item}
-                onChange={() => setFruite(item)}
-              /> {item}
-            </label>
-          </li>
-        ))}
-      </ul>
-      <br />
-      {fruit}
-      <hr />
-      <br />
-      <select
-        onChange={(e) => handlerSelect(e)}>
-        {fruits.map((item, index) => (
-          <option key={item} value={item}>
-            {item}
-          </option>
-        ))}
-      </select>
-      <br />
-      {fruitSelect}
+      <City
+        city={currentCity}
+        onChangeCity={handlerChangeCity}
+        currentIndex={currentIndex} />
+      <CitiesList cities={cities} onSelectCity={handlerselectCity} />
     </div>
   );
 }
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//
-//     this.state = {
-//       value: "Some text 2"
-//     };
-//   }
-//
-//   handlerOnChange(event) {
-//     this.setState({
-//       value: event.target.value
-//     });
-//   }
-//
-//   render () {
-//     return (
-//       <div className="App">
-//         <input value={this.state.value} onChange={(e) => this.handlerOnChange(e)} />
-//         {this.state.value}
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
