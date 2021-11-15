c2293af0575c164207c64ed6328f2ee19fd9dbbf
Maxim <maxidebox@list.ru>
2021-11-15 10:27:22 +0400

    Рендринг списка

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const users = [
+    {
+      id: 0,
+      name: "Maxim",
+      surname: "Pak"
+    },
+    {
+      id: 1,
+      name: "Dima",
+      surname: "Pak"
+    },
+    {
+      id: 2,
+      name: "Nata",
+      surname: "Pak"
+    },
+    {
+      id: 3,
+      name: "Andrey",
+      surname: "Pak"
+    },
+    {
+      id: 4,
+      name: "Igor",
+      surname: "Pak"
+    },
+  ];
+
+  const handlerMouseEnter = (user) => {
+    console.log(user.name);
+  }
+
+  let list = users.map((user, index) => {
+    const even = index % 2 === 0;
+    let style = {};
+
+    if (even) {
+      style = {
+        color: "white",
+        background: "black"
+      }
+    }
+
+    return (
+      <li key={user.id} style={style} onMouseEnter={() => handlerMouseEnter(user)}>
+        {user.name} {user.surname}
+      </li>
+    )
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {list}
+    </ul>
   );
 }
 
