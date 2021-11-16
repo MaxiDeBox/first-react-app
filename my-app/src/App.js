de7cde22b442eb9f87538c12ca100466e9f6d432
Maxim <maxidebox@list.ru>
2021-11-16 11:27:09 +0400

    Хук useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+// function App() {
+//   const [counters, setCounters] = useState([0, 0, 0]);
+//
+//   const count = (n) => {
+//     return setCounters(
+//       counters.map((v, i) => {
+//         return (n === i) ? v + 1 : v;
+//       })
+//     );
+//   }
+//
+//   return (
+//     <div className="App">
+//       <ul>
+//         {
+//           counters.map((item, index) => (
+//             <li key={index}>
+//               <button onClick={() => count(index)}> Clicked {counters[index]} </button>
+//             </li>
+//           ))
+//         }
+//       </ul>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   const [state, setState] = useState({
+//     counter: 0,
+//     user: {
+//       name: "Maxim",
+//       surname: "Pak"
+//     }
+//   });
+//
+//   const handlerClick = () => {
+//     console.log(state);
+//     setState({
+//        counter: state.counter + 1,
+//         ...state
+//     })
+//   }
+//
+//   return (
+//     <div className="App">
+//       <button onClick={handlerClick}>Click</button>
+//     </div>
+//   )
+// }
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const handlerClick = () => {
+    setCounter((x, props) => x + 1);
+    setCounter((x, props) => x + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handlerClick}>Clicked {counter}</button>
     </div>
-  );
+  )
 }
 
 export default App;
