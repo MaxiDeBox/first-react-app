bac27f62623b70668a585e97ce0da52d4f4973d8
Maxim <maxidebox@list.ru>
2021-11-24 20:57:07 +0400

    Управляемые и неуправляемые компоненты

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import {useCallback, useEffect, useRef} from "react";
+
+const ENTER = "Enter";
+
+// function App() {
+//   const ref = useRef();
+//
+//   const handlerClick = useCallback(() => {
+//     console.log(ref.current.value)
+//   }, [ref]);
+//
+//   const handlerKeyup = useCallback((event) => {
+//     const {key} = event;
+//
+//     if (key === ENTER) {
+//       handlerClick();
+//     }
+//
+//   }, [handlerClick])
+//
+//   useEffect(() => {
+//     if(ref.current) {
+//       ref.current.addEventListener("keyup", handlerKeyup);
+//
+//       return () => {
+//         ref.current.removeEventListener("keyup", handlerKeyup);
+//       };
+//     }
+//   }, [handlerKeyup]);
+//
+//   return (
+//     <div className="App">
+//       <input ref={ref} />
+//       <button onClick={handlerClick}>Send</button>
+//     </div>
+//   );
+// }
+
 
 function App() {
+  const userName = useRef("");
+  const userSurname = useRef("");
+
+  const onButtonClick = useCallback(() => {
+    const user = {
+      name: userName.current.value,
+      surname: userSurname.current.value
+    };
+
+    if(!userName.current.value) {
+      userName.current.focus();
+    }
+
+    if(!userSurname.current.value) {
+      userSurname.current.focus();
+    }
+
+    if(userSurname.current.value && userName.current.value) {
+      console.log(user);
+    }
+  }, [userName, userSurname]);
+
+  const onKeyUp = useCallback((event) => {
+    const {key} = event;
+
+    if (key === ENTER) {
+      onButtonClick();
+    }
+
+  }, [onButtonClick]);
+
+  useEffect(() => {
+    if(userName.current && userSurname.current) {
+      userName.current.addEventListener("keyup", onKeyUp);
+      userSurname.current.addEventListener("keyup", onKeyUp);
+    }
+
+    return () => {
+      userName.current.removeListener("keyup", onKeyUp);
+      userSurname.current.removeListener("keyup", onKeyUp);
+    }
+  }, [onKeyUp]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <input ref={userName} type="text" placeholder="Your name" />
+      </p>
+      <p>
+        <input ref={userSurname} type="text" placeholder="Your surname" />
+      </p>
+      <button onClick={onButtonClick}>Send</button>
     </div>
   );
 }
