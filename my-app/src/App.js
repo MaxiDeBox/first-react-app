15ad0bcff55872a643fcbb02e0980568010998c5
Maxim <maxidebox@list.ru>
2021-11-28 13:56:00 +0400

    @redux/toolkit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { add, remove, doneToggle } from "./store/todo";
+import { useDispatch, useSelector } from "react-redux";
+
 
 function App() {
+  const items = useSelector((state) => state.todo);
+  const dispatch = useDispatch();
+
+  const [value, setValue] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={() => dispatch(add(value))}>Добавить элемент</button>
+      <br />
+      <ul>
+        { items.map((item) => (
+            <li key={item.id}>
+              <button onClick={() => dispatch(doneToggle(item.id))}>Done</button>
+              <button onClick={() => dispatch(remove(item.id))}>Remove</button>
+              <span className={item.done ? "done" : ""}>{item.content}</span>
+            </li>
+          ))}
+      </ul>
+
     </div>
   );
 }
 
 export default App;
+
+
