30e258e8462365bb8383d5ea60cc42908855f6a3
Maxim <maxidebox@list.ru>
2021-11-28 14:33:55 +0400

    react-router-dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,19 @@
-import './App.css';
-import { useState } from "react";
-import { add, remove, doneToggle } from "./store/todo";
-import { useDispatch, useSelector } from "react-redux";
+import "./styles.css";
 
+import { Switch, Route } from "react-router-dom";
+import { UserPage, MainPage } from "./pages";
 
-function App() {
-  const items = useSelector((state) => state.todo);
-  const dispatch = useDispatch();
-
-  const [value, setValue] = useState("");
-
+export default function App() {
   return (
-    <div className="App">
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <button onClick={() => dispatch(add(value))}>Добавить элемент</button>
-      <br />
-      <ul>
-        { items.map((item) => (
-            <li key={item.id}>
-              <button onClick={() => dispatch(doneToggle(item.id))}>Done</button>
-              <button onClick={() => dispatch(remove(item.id))}>Remove</button>
-              <span className={item.done ? "done" : ""}>{item.content}</span>
-            </li>
-          ))}
-      </ul>
+    <Switch>
+      <Route exact path="/user/:userId" component={UserPage} />
 
-    </div>
+      <Route
+        path="*"
+        render={() => {
+          return <MainPage />;
+        }}
+      />
+    </Switch>
   );
 }
-
-export default App;
-
-
